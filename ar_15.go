60c0ea3497852d48960e3dcfebece734b9c94123
Medokser <alaziz.200223@gmail.com>
2023-02-05 22:43:58 +0600

    v1.1.0

--- ar_15.go
+++ ar_15.go
@@ -1,31 +1,38 @@
 package mypackage
 
+import "fmt"
 
-type Ar struct{
+type Ar struct {
 	Magazine_capacity int
-	Material string
-	Scope bool
-	Scope_zoom int
+	Material          string
+	Scope             bool
+	Scope_zoom        int
 }
-func NewAr() *Ar{
+
+func NewAr() *Ar {
 	return &Ar{Magazine_capacity: 30, Material: "titan", Scope: false, Scope_zoom: 1}
 }
-func AddMagazineCapacity(ar *Ar, newAmount int){
+func AddMagazineCapacity(ar *Ar, newAmount int) {
 	ar.Magazine_capacity = newAmount
 }
-func AddScope(ar *Ar, zoom int){
+func AddScope(ar *Ar, zoom int) {
 	if !ar.Scope {
-		ar.Scope = true;
-		ar.Scope_zoom = zoom;
+		ar.Scope = true
+		ar.Scope_zoom = zoom
+		return
 	}
+	fmt.Println("Error: You already have scope!")
 }
-func ChangeScope(ar *Ar, zoom int){
+func ChangeScope(ar *Ar, zoom int) {
 	if ar.Scope {
 		if zoom < 2 {
+			fmt.Println("Scope removed!")
 			ar.Scope = false
 			ar.Scope_zoom = 1
 			return
 		}
 		ar.Scope_zoom = zoom
+		return
 	}
+	fmt.Println("Error: You dont have scope!")
 }
